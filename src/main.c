904b113a1c78e9f0b9b38bd125f0afa48b151f6a
Jan Šipr <sipr.jan@gmail.com>
2014-08-24 20:19:03 +0200

    Add small example of lcd log.
    Assert function used by our library need assert failure function which describe
    what to do or where to print fault message. We can use it to demonstrate usage
    of lcd logging framework provided by stm. Be aware of hw dependability of this
    solution, the initialization of lcd can change from board to board.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,35 @@
-void main()
+#include <stdint.h>
+#include "stm32f429i_discovery.h"
+#include "stm32f429i_discovery_lcd.h"
+#include "lcd_log.h"
+
+
+int main()
 {
-    return;
+    return 0;
 }
+
+void assert_failed(uint8_t* file, uint32_t line)
+{
+    LCD_Init();
+
+    /* LCD Layer initialization */
+    LCD_LayerInit();
+
+    /* Enable the LTDC */
+    LTDC_Cmd(ENABLE);
+
+    /* Set LCD foreground layer */
+    LCD_SetLayer(LCD_FOREGROUND_LAYER);
+
+    /* Clear the LCD */
+    LCD_Clear(LCD_COLOR_WHITE);
+
+    LCD_LOG_Init();
+    LCD_LOG_SetHeader((uint8_t*)"header");
+    LCD_LOG_SetFooter((uint8_t*)"footer");
+
+    LCD_ErrLog("%s: %d\n", file, (int)line);
+    while(1);
+}
+
